991c7035caa49c5bc79389b5758c5a118ea89397
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-06-05 19:16:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import praw
 import time
 import os
 
-bot = Bot(os.environ.get("BOT_TOKEN"))
+bot = Bot("1833090950:AAEUdZDfXcN1K39guH7jMiI8z4hVR3IJhBI")
 reddit = praw.Reddit(client_id="ZzYynlO2tygKWg", client_secret="BT6frmh3NWZbGzm-YI33CU9xZcKiWA", user_agent="telegrambot")
-updater=Updater(os.environ.get("BOT_TOKEN"),use_context=True)
+updater=Updater("1833090950:AAEUdZDfXcN1K39guH7jMiI8z4hVR3IJhBI"),use_context=True)
 
 dispatcher=updater.dispatcher
 
@@ -14,7 +14,7 @@ def test_function (update:Update,context:CallbackContext):
     last = ''
     a = 1
     while a < 50000:
-        subred = reddit.subreddit("hentai")
+        subred = reddit.subreddit("ecchi")
         new = subred.new(limit = 1)
         for i in new:
             if i.url != last:
@@ -25,13 +25,13 @@ def test_function (update:Update,context:CallbackContext):
                     ]]
                 
                 try:
-                    bot.sendPhoto(chat_id= 732913305, caption= i.title, photo=i.url, reply_markup = InlineKeyboardMarkup(keyboard))
+                    bot.sendPhoto(chat_id= 1292443145, caption= i.title, photo=i.url, reply_markup = InlineKeyboardMarkup(keyboard))
                     last = i.url
                     
                 except Exception as e:
                     print(e)
                     print(i.url)
-                    bot.send_message(chat_id= 732913305, text= f"{i.title}\n{i.url}", reply_markup = InlineKeyboardMarkup(keyboard))
+                    bot.send_message(chat_id= 1292443145, text= f"{i.title}\n{i.url}", reply_markup = InlineKeyboardMarkup(keyboard))
 
         a = a + 1
         print(a)
