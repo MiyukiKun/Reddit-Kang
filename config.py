154b57f0d4316baa470e03c02b57885450cb98bc
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-06-10 11:55:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ from telethon import TelegramClient
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
+reddit_personal_use_script = os.environ.get('REDDIT_PERSONAL_USE_SCRIPT')
+reddit_secret = os.environ.get('REDDIT_SECRET')
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
