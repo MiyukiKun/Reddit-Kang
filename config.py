e100933f13e9b70767fa16671f7e16f906961e37
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-06-10 12:01:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,8 @@ api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
 reddit_personal_use_script = os.environ.get('REDDIT_PERSONAL_USE_SCRIPT')
 reddit_secret = os.environ.get('REDDIT_SECRET')
+agent = os.environ.get('AGENT')
+subreddit = os.environ.get('SUBREDDIT')
+channel_id = os.environ.get('CHANNEL_ID')
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
